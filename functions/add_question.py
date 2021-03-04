a99ffdc93959250e74bdc5784d744475b9431b04
zaphodnothingth <zaphodnothingth@gmail.com>
2021-03-04 00:27:12 -0500

    initial load of add_question.py

---
+++ functions/add_question.py
@@ -0,0 +1,69 @@
+''' this module interacts with firestore  - https://firebase.google.com/docs/firestore/manage-data/add-data
+example manaul load, auto generate id:
+question = SurveyQuestion(text=u'How do you feel?', qtype=u'likert')
+db.collection(u'survey_questions').add(city.to_dict())
+
+to set creds on powershell :  # https://cloud.google.com/docs/authentication/getting-started#windows
+env:GOOGLE_APPLICATION_CREDENTIALS="./cultural engagement-f2d38a123f26.json"
+'''
+
+import argparse
+import sys
+from csv import reader
+from google.cloud import firestore
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description='arguments to provide to main()',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('-m', '--mode', default='csv')
+    parser.add_argument('-t', '--target')
+
+    return parser.parse_args(args)
+
+class SurveyQuestion(object):
+    def __init__(self, text, qtype):
+        self.text = text
+        self.qtype = qtype
+        
+    @staticmethod
+    def from_dict(source):
+        question = SurveyQuestion(source[u'text'], source[u'qtype'])
+
+        return question
+
+    def to_dict(self):
+        dest = {
+            u'text': self.text,
+            u'qtype': self.qtype
+        }
+
+        return dest
+
+    def __repr__(self):
+        return(
+            f'City(\
+                text={self.text}, \
+                qtype={self.qtype}, \
+            )'
+        )
+        
+
+def main(args):
+    pargs = parse_args(args)
+    db = firestore.Client()
+    if pargs.mode == 'csv':
+        with open(pargs.target, 'r') as read_obj:
+            csv_reader = reader(read_obj, delimiter='\t')
+            headers = next(csv_reader)
+            for row in csv_reader:
+                question = SurveyQuestion(
+                text=row[0],
+                qtype=row[1])
+                db.collection(u'survey_questions').add(question.to_dict())
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
